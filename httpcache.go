b744373aa80463f2d15b26c27fc639eafb235efc
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-09-09 10:43:15 +0200

    Memory cache backed by lru

--- httpcache.go
+++ httpcache.go
@@ -18,6 +18,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/cozy/httpcache/lru"
 )
 
 const (
@@ -59,13 +61,13 @@ func CachedResponse(c Cache, req *http.Request) (resp *http.Response, err error)
 // MemoryCache is an implemtation of Cache that stores responses in an in-memory map.
 type MemoryCache struct {
 	mu    sync.RWMutex
-	items map[string][]byte
+	items *lru.Cache
 }
 
 // Get returns the []byte representation of the response and true if present, false if not
 func (c *MemoryCache) Get(key string) (resp []byte, ok bool) {
 	c.mu.RLock()
-	resp, ok = c.items[key]
+	resp, ok = c.items.Get(lru.Key(key))
 	c.mu.RUnlock()
 	return resp, ok
 }
@@ -73,20 +75,20 @@ func (c *MemoryCache) Get(key string) (resp []byte, ok bool) {
 // Set saves response resp to the cache with key
 func (c *MemoryCache) Set(key string, resp []byte) {
 	c.mu.Lock()
-	c.items[key] = resp
+	c.items.Add(lru.Key(key), resp)
 	c.mu.Unlock()
 }
 
 // Delete removes key from the cache
 func (c *MemoryCache) Delete(key string) {
 	c.mu.Lock()
-	delete(c.items, key)
+	c.items.Remove(lru.Key(key))
 	c.mu.Unlock()
 }
 
 // NewMemoryCache returns a new Cache that will store items in an in-memory map
 func NewMemoryCache() *MemoryCache {
-	c := &MemoryCache{items: map[string][]byte{}}
+	c := &MemoryCache{items: lru.New(20)}
 	return c
 }
 
